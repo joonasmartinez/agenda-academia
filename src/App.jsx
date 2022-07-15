1b9f9171351587ed70b08edd175f7a02445feffc
Jonas <mz-martinez@live.com>
2022-07-15 13:57:18 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h3>Hello World</h3>
+      <p>Agende seu horário.</p>
+    </div>
+  )
+}
+
+export default App
